2d44d9e320ff07b5295caf60d2c7e5f205f3fa40
romildoalimajr <odlimor.sevla@gmail.com>
2022-10-23 00:15:43 -0300

    modified:   lib/main.dart 	modified:   lib/questao.dart 	modified:   lib/questionario.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,111 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
+import './resultado.dart';
+import './questionario.dart';
 
 main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
 
-  void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
-    print(_perguntaSelecionada);
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita? ',
+      'respostas': [
+        {'texto': 'Azul', 'pontuação': 10},
+        {'texto': 'Vermelho', 'pontuação': 7},
+        {'texto': 'Preto', 'pontuação': 5},
+        {'texto': 'Amarelo', 'pontuação': 3},
+        {'texto': 'Rosa', 'pontuação': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Cachorro', 'pontuação': 5},
+        {'texto': 'Gato', 'pontuação': 3},
+        {'texto': 'Cavalo', 'pontuação': 7},
+        {'texto': 'Lobo', 'pontuação': 9},
+        {'texto': 'Leão', 'pontuação': 10},
+      ],
+    },
+    {
+      'texto': 'Qual e a sua liguagem de programação favorita?',
+      'respostas': [
+        {'texto': 'Dart', 'pontuação': 10},
+        {'texto': 'Java', 'pontuação': 9},
+        {'texto': 'C++', 'pontuação': 7},
+        {'texto': 'PHP', 'pontuação': 3},
+        {'texto': 'JavaScript', 'pontuação': 5},
+      ],
+    },
+    {
+      'texto': 'Qual é a sua série de TV favorita?',
+      'respostas': [
+        {'texto': 'The Big Bang Theory', 'pontuação': 10},
+        {'texto': 'Two and Half Man', 'pontuação': 5},
+        {'texto': 'The Middle', 'pontuação': 6},
+        {'texto': 'The Walking Dead', 'pontuação': 9},
+        {'texto': 'Chaves & Chapolin', 'pontuação': 7},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu filme favorito?',
+      'respostas': [
+        {'texto': 'The Matrix', 'pontuação': 10},
+        {'texto': 'Mission: Impossible', 'pontuação': 9},
+        {'texto': 'The Vast Night', 'pontuação': 8},
+        {'texto': 'Star Trek', 'pontuação': 7},
+        {'texto': 'Star Wars', 'pontuação': 5},
+      ],
+    },
+    {
+      'texto': 'Qual a sua cidade favorita?',
+      'respostas': [
+        {'texto': 'Recife', 'pontuação': 5},
+        {'texto': 'Lisboa', 'pontuação': 6},
+        {'texto': 'Paris', 'pontuação': 7},
+        {'texto': 'Londres', 'pontuação': 9},
+        {'texto': 'New York', 'pontuação': 10},
+      ],
+    },
+  ];
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+    print(_pontuacaoTotal);
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      'Qual é a sua cor favorita? ',
-      'Qual é o seu animal favorito?',
-      'Qual e a sua liguagem de programação favorita?',
-      'Qual é a sua série de TV favorita?',
-      'Qual é o seu filme favorito?',
-      'Qual a sua cidade favorita?',
-    ];
+    //List<Widget> widgets = respostas.map((t) => Resposta(t, _responder)).toList();
+
+    /*for (String textoResp in respostas) {
+      print(textoResp);
+      widgets.add(Resposta(textoResp, _responder));
+    }*/
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(child: Text('Resposta 01'), onPressed: _responder),
-            ElevatedButton(child: Text('Resposta 02'), onPressed: _responder),
-            ElevatedButton(child: Text('Resposta 03'), onPressed: _responder),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
+              )
+            : Resultado(),
       ),
       debugShowCheckedModeBanner: false,
     );
